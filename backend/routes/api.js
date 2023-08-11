b30fa93cd318528e84eb861e154f142a3e0890af
usaid <mdusaid922@gmail.com>
2023-08-12 02:55:23 +0500

    removed notes schema from everywhere

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,12 +3,12 @@ var PharmacyRouter=require('./pharmacy');
 var BloodRouter=require('./blood');
 var DoctorRouter=require('./doctor');
 var HospitalRouter=require('./hospital');
-var NoteRouter =require("./note");
+
 var TransactionRouter=require("./transaction");
 var app = express();
 
 
-app.use('/note',NoteRouter);
+
 app.use('/pharmacy',PharmacyRouter);
 app.use('/hospital',HospitalRouter);
 app.use('/doctor',DoctorRouter);
