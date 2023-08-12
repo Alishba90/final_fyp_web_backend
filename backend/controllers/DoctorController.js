96d13e38a77409110472f0bf897e5683d24b0d82
usaid <mdusaid922@gmail.com>
2023-08-12 20:11:54 +0500

    solved and merged zainab ka function

--- backend/controllers/DoctorController.js
+++ backend/controllers/DoctorController.js
@@ -23,23 +23,21 @@ function DoctorData(data) {
 exports.DoctorDetail = [
 
 	async (req, res) => {
-    console.log('this is recieve',req.body)
+    console.log('this is recieveeddddddd',req.body)
     try{
-        await Doctor.findOne({Name:req.params.name , Email:req.params.email}).then(dr=>{
-                if(dr){
-                        
-                    var doctor=new DoctorData(dr[0])
+        const { name, email } = req.params;
 
-                    return res.status(200).send({org:doctor});
-                }
-                else{
-                    return res.status(430).send({ null_data:"No such doctor found"});
-                }    
-            })
+        const doctor = await Doctor.findOne({ Name: name, Email: email });
+    
+        if (doctor) {
+          return res.status(200).json({ doctor });
+        } else {
+          return res.status(404).json({ error: 'Doctor not found' });
+        }
     }
     catch(err){
-        console.log(err);
-        return res.status(430).send({ error: err});
+        console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
     }
     }
 ];
@@ -77,21 +75,24 @@ exports.AddDoctor = [
     }
 ];
 
-//Update doctor
+//update doctor information
 exports.UpdateDoctor = [
-
-	(req, res) => {
-    console.log('this is recieve',req.body)
-    try{
-		var doctor=new Doctor({
-                Name:req.body.doctorinfo.name,
-                Education:req.body.doctorinfo.education,
-                Speciality:req.body.doctorinfo.speciality,
-                Experience:req.body.doctorinfo.experience,
-                Email:req.body.doctorinfo.email,
-                Phone:req.body.doctorinfo.phone,
-                password:req.body.doctorinfo.password,
-            })
+    (req, res) => {
+      console.log('Received update request:', req.body);
+      try {
+        const updateFields = {
+          Name: req.body.doctorinfo.Name,
+          Education: req.body.doctorinfo.Education,
+          Speciality: req.body.doctorinfo.Speciality,
+          Experience: req.body.doctorinfo.Experience,
+          Email: req.body.doctorinfo.Email,
+          Phone: req.body.doctorinfo.Phone,
+          password: req.body.doctorinfo.password,
+          Hospitals: req.body.doctorinfo.Hospitals, // Add updated hospitals here
+        };
+  
+        console.log('Updating doctor with:', updateFields);
+  
         Doctor.findOneAndUpdate(
 				{Name:req.body.old_name , Email:req.body.old_email},
 				doctor
@@ -107,8 +108,8 @@ exports.UpdateDoctor = [
     }
     catch(err){
         console.log(err);
-        return res.status(430).send({ error: err});
-    }
+        return res.status(430).send({ error: err });
+      }
     }
 ];
 
@@ -224,12 +225,14 @@ exports.DoctorSchedule = [
 exports.DoctorScheduleEdit = [
 	async (req, res) => {
     console.log('this is recieved',req.params)
+    console.log('this is recieved',req.params)
     try{
         await Doctor.findOne({Name:req.params.name , Email:req.params.email}).then(dr=>{
                 if(dr){
                        
                     var doctor=new DoctorData(dr)
                     var schedule=doctor.Hospitals
+                    
                     
                     
                     return res.status(200).send({schedule:schedule});
@@ -249,7 +252,10 @@ exports.DoctorScheduleEdit = [
 exports.DoctorAppointments = async (req, res) => {
     console.log('this is receive app', req.params);
     try {
-        /*const dr = await Doctor.findOne({ Name: req.params.name, Email: req.params.email });*/
+        const { name, email } = req.params;
+
+        // const doctor = await Doctor.findOne({ Name: name, Email: email });
+        // /const dr = await Doctor.findOne({ Name: req.params.name, Email: req.params.email });/
         const dr = await Doctor.findOne({ Name: 'Dr Zainabb', Email: 'SHAEENkhan90@gmail.com' });
         if (dr) {
             const appointments = await Appointment.find({ doctorId: dr._id }).exec();
@@ -264,6 +270,17 @@ exports.DoctorAppointments = async (req, res) => {
     }
 };
 
+exports.DoctorChart=[
+    (req, res) => {
+     
+        try {
+        }
+        catch(err){
+            console.log(err)
+            return res.status(430).json({ error: err });
+        }
+    }
+]
 exports.DoctorChart=[
     (req, res) => {
      
